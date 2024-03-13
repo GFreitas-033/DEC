ce74af40c483eb62d5411dd82f5f16a45d83bd38
unknown <adventuresaulo@gmail.com>
2024-03-13 20:50:46 -0300

    Atualização da conexão

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -3,9 +3,17 @@ const {
 } = require("mysql");
 
 const pool = createPool({
-    host: "",
+    host: "localhost",
     user: "",
     password: "",
     database: "",
     connectionLimit: 10
-})
+});
+
+// pool.query('select * from noticia', (err, result, fields) => {
+//     if(err){
+//         return console.log(err);
+//     }
+//     return console.log(result);
+// })
+
