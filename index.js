5f4ab686ac73db874d6c4ccdcdfba00b32a7c50f
unknown <adventuresaulo@gmail.com>
2024-03-16 17:31:56 -0300

    Correção de erros: Arquivos do servidor

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-const path = require('path');
-
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, './frontend/index.html'));
 });
 
 app.listen(port,()=>{
