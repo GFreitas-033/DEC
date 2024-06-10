0b192f14193c3f48761bd8606c198108aaaf55aa
saulobbarbosa <adventuresaulo@gmail.com>
2024-06-10 10:56:41 -0300

    Atualizacao proxy

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,25 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/login',
+    createProxyMiddleware({
+      target: 'http://localhost:5000/login',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/sair',
+    createProxyMiddleware({
+      target: 'http://localhost:5000/sair',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000/api',
+      changeOrigin: true,
+    })
+  );
+};
