486a320d7650853b5263658df4a92b096b10ca7d
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2024-06-16 19:47:51 -0300

    Mascara no Telefone na tela de Cadastro

--- frontend/src/components/cadastro/inputs_cadastro/telefone_input.js
+++ frontend/src/components/cadastro/inputs_cadastro/telefone_input.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React,{useState} from "react"
 import Telefone from "./input.module.css"
 
 export default function Telefone_input(){
+    const [telefone, setTelefone] = useState("")
+
+    function formatarTel(event) {
+        let t = event.target.value
+        t = t.replace(/\D+/g, "")
+        t = t.replace(/(\d{2})(\d)/, "($1) $2")
+        t = t.replace(/(\d{5})(\d)/, "$1-$2")
+        if(t.length>15){
+            t = t.substring(0,15)
+        }
+        setTelefone(t)
+    }
     return(
         <div className={Telefone.esquerda}>
             <label className={Telefone.label}><b>Telefone</b></label><br />
-            <input type="text" id="Telefone" placeholder="Coloque seu Telefone" required className={Telefone.input}/><br />
+            <input type="text" id="Telefone" placeholder="Coloque seu Telefone" required className={Telefone.input}
+            onChange={formatarTel}
+            value={telefone}/><br />
         </div>
     )
 }
