7bbf91257ee38926b53a574ccfef15c6a1e0eaaf
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2024-06-16 18:49:22 -0300

    Mascara no RG na tela de Cadastro

--- frontend/src/components/cadastro/inputs_cadastro/rg_input.js
+++ frontend/src/components/cadastro/inputs_cadastro/rg_input.js
@@ -1,11 +1,26 @@
-import React from "react"
+import React,{useState} from "react"
 import Rg from "./input.module.css"
 
 export default function Rg_input(){
+    const [rg, setRg] = useState("")
+
+    function formatarRG(event) {
+        let r = event.target.value
+        r = r.replace(/\D+/g, "")
+        r = r.replace(/^(\d{2})(\d)/, "$1.$2")
+        r = r.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+        r = r.replace(/^(\d{2})\.(\d{3})\.(\d{3})(\d{1})/, "$1.$2.$3-$4")
+        if(r.length>12){
+            r = r.substring(0,12)
+        }
+        setRg(r)
+    }
     return(
         <div className={Rg.esquerda}>
             <label className={Rg.label}><b>RG</b></label><br />
-            <input type="text" id="Rg" placeholder="Coloque seu Rg" required className={Rg.input}/><br />
+            <input type="text" id="Rg" placeholder="Coloque seu Rg" required className={Rg.input}
+            onChange={formatarRG}
+            value={rg}/><br />
         </div>
     )
 }
