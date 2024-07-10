1e7e54bb899985b79e14b72998aedbe44be7585e
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2024-07-10 14:15:08 -0300

    Coloquei os Icons no nomes

--- frontend/src/components/adm/adm_aluno/content_adm_aluno.js
+++ frontend/src/components/adm/adm_aluno/content_adm_aluno.js
@@ -1,29 +1,53 @@
 import React from "react"
 import AdmAluno from "./admAluno.module.css"
 
+import Editar from "../../../imgs/Editar.png"
+import Excluir from "../../../imgs/Excluir.png"
+
 export default function content_adm_aluno(){
     return(
         <div className={AdmAluno.contentAdmAluno}>
-            <div className={AdmAluno.divTitulo}>
+            <div>
                 <h1 className={AdmAluno.titulo}>ALUNOS</h1>
             </div>
 
             <div className={AdmAluno.tabela}>
-                <div className={AdmAluno.linhas}>
+
+                <div className={AdmAluno.linhasId}>
+
                     <ul className={AdmAluno.colunaID}>
                         <li className={AdmAluno.topoTable}><b><u>ID</u></b></li>
+
                         <li>1</li>
                         <li>2</li>
                         <li>3</li>
                     </ul>
+                    
                 </div>
-                <div className={AdmAluno.linhas}>
+
+                <div className={AdmAluno.linhasNome}>
                     <ul>
                         <li className={AdmAluno.topoTable}><b><u>Nome</u></b></li>
-                        <li>Samuel</li>
-                        <li>André</li>
-                        <li>Teste</li>
+                        
+                        <li>
+                            Samuel
+                            <img src={Excluir} className={AdmAluno.iconAluno}/>
+                            <img src={Editar} className={AdmAluno.iconAluno}/>
+                        </li>
+
+                        <li>
+                            André
+                            <img src={Excluir} className={AdmAluno.iconAluno}/>
+                            <img src={Editar} className={AdmAluno.iconAluno}/>
+                        </li>
+
+                        <li>
+                            Teste
+                            <img src={Excluir} className={AdmAluno.iconAluno}/>
+                            <img src={Editar} className={AdmAluno.iconAluno}/>
+                        </li>
                     </ul>
+
                 </div>
             </div>
         </div>
