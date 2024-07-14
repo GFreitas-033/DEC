bc7cd04eccb73a47888f556f6806c01eced281ab
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2024-07-14 15:42:00 -0300

    Cadastro do Responsavel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Routers, Routes, Route, Link} from "react-router-dom"
 // Import das Telas
 import Login from "./components/login/form_login/form"
 import Cadastro from "./components/cadastro/form_cadastro/form"
+import CadastroResponsavel from "./components/cadastroResponsavel/cadastroResponsavel"
 import Home from "./components/home/home"
 import Calendario from "./components/calendario/calendario"
 import Adm from "./components/adm/adm"
@@ -21,6 +22,7 @@ export default function App(){
         <Link to="/"></Link>
         <Link to="/home"></Link>
         <Link to="/cadastro"></Link>
+        <Link to="/cadastro/responsavel"></Link>
         <Link to="/calendario"></Link>
         <Link to="/adm"></Link>
         <Link to="calendario/alunosCalendario"></Link>
@@ -34,9 +36,11 @@ export default function App(){
         <Route path="/" element={<Login />}></Route>
         <Route path="/home" element={<Home/>}></Route>
         <Route path="/cadastro" element={<Cadastro />}></Route>
+        <Route path="/cadastro/responsavel" element={<CadastroResponsavel />}></Route>
         <Route path="/calendario" element={<Calendario />}></Route>
         <Route path="/adm" element={<Adm />}></Route>
         <Route path="calendario/alunosCalendario/:idturma" element={<Alunos />}></Route>
+        
         <Route path="adm/adm_aluno" element={<AdmAluno />}></Route>
         <Route path="adm/adm_prof" element={<AdmProf />}></Route>
         <Route path="adm/adm_unidade" element={<AdmUni />}></Route>
---
+++ frontend/src/components/cadastroResponsavel/cadastroResponsavel.js
@@ -0,0 +1,36 @@
+import React,{useState} from "react"
+import Styles from "../cadastro/form_cadastro/form.module.css"
+
+import Texto from "../cadastro/textos_cadastro/texto_cadastro"
+
+// Import dos Input
+import Email from "../cadastro/inputs_cadastro/email_input"
+import Senha from "../cadastro/inputs_cadastro/senha_input"
+import Nome from "../cadastro/inputs_cadastro/nome_input"
+import CPF from "../cadastro/inputs_cadastro/cpf_input"
+import RG from "../cadastro/inputs_cadastro/rg_input"
+import Telefone from "../cadastro/inputs_cadastro/telefone_input"
+import Dt_nasc from "../cadastro/inputs_cadastro/dt_nasc_input"
+// Import Botão
+import Botao from "../cadastro/botao_cadastro/submit_cadastro"
+
+export default function FormCadastroResponsavel(){
+
+  return(
+    <div className={Styles.container_formcadastro}>
+      <form className={Styles.form} autoComplete="off">
+        <Texto />
+        <div className={Styles.container_inputs}>
+          <Email />
+          <Senha />
+          <Nome />
+          <CPF />
+          <RG />
+          <Telefone />
+          <Dt_nasc />
+        </div>
+        <Botao />
+      </form>
+    </div>
+  )
+}
