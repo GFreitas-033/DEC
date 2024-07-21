a0f1bdc8b4dc00e49b4bed2fc516705402b88db0
saulobbarbosa <adventuresaulo@gmail.com>
2024-07-20 21:40:27 -0300

    Comentario para exclusao de pessoa (Descomentar depois)

--- backend/controllers/admController.js
+++ backend/controllers/admController.js
@@ -27,6 +27,7 @@ router.get('/excluiraluno/:id_aluno', async (req, res) => {
     try {
         await axios.delete(`http://localhost:5000/api/aluno_has_turma/${id_aluno}`);
         await axios.delete(`http://localhost:5000/api/aluno/${id_aluno}`);
+        // await axios.delete(`http://localhost:5000/api/pessoa/${id_aluno}`);
         res.status(200).send('Registro excluído com sucesso!');
     } catch (error) {
         console.error(error);
