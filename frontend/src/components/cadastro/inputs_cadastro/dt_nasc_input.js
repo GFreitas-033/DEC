99301c47e06039bfd86a7474380e07fabafbea0a
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2024-10-05 14:50:45 -0300

    Ajuste no tamanho do input

--- frontend/src/components/cadastro/inputs_cadastro/dt_nasc_input.js
+++ frontend/src/components/cadastro/inputs_cadastro/dt_nasc_input.js
@@ -4,7 +4,7 @@ import Dt_nasc from "./input.module.css"
 export default function Dt_nasc_input(){
     return(
         <div className={Dt_nasc.esquerda}>
-            <label className={Dt_nasc.label}><b>Data de Nascimento</b></label><br />
+            <label className={Dt_nasc.label}><b>Data Nascimento</b></label><br />
             <input type="date" id="dt_nasc" required className={Dt_nasc.input}/><br />
         </div>
     )
