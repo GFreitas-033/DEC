7d52978eb4f6a6e70807579a20e23d03a247ded2
saulobbarbosa <adventuresaulo@gmail.com>
2024-11-02 19:21:26 -0300

    Correção de máscaras

--- frontend/src/components/cadastro/form_cadastro/form.js
+++ frontend/src/components/cadastro/form_cadastro/form.js
@@ -18,10 +18,37 @@ import UF from "../inputs_cadastro/endereco/uf_input";
 import Cidade from "../inputs_cadastro/endereco/cidade_input";
 import Bairro from "../inputs_cadastro/endereco/bairro_input";
 import Rua from "../inputs_cadastro/endereco/rua_input";
-// Continue importando os outros inputs...
 
 export default function Form() {
   const [step, setStep] = useState(0);
+  const [logradouro, setLogradouro] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [uf, setUf] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [rg, setRg] = useState("");
+  const [telefone, setTelefone] = useState("");
+
+  const handleBuscarCep = (cep) => {
+    if (cep.length < 9) {
+      setLogradouro("");
+      setBairro("");
+      setCidade("");
+      setUf("");
+      return;
+    }
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((response) => response.json())
+      .then((dados) => {
+        setLogradouro(dados.logradouro);
+        setBairro(dados.bairro);
+        setCidade(dados.localidade);
+        setUf(dados.uf);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar CEP:', error);
+      });
+  };
 
   const nextStep = () => {
     setStep((prevStep) => Math.min(prevStep + 1, steps.length - 1));
@@ -32,10 +59,17 @@ export default function Form() {
   };
 
   const steps = [
-    <Passo1 nextStep={nextStep} />,
-    <Passo2 nextStep={nextStep} prevStep={prevStep} />,
-    <Passo3 nextStep={nextStep} prevStep={prevStep} />,
-
+    <Passo1 nextStep={nextStep} rg={rg} setRg={setRg} cpf={cpf} setCpf={setCpf} />,
+    <Passo2 nextStep={nextStep} prevStep={prevStep} telefone={telefone} setTelefone={setTelefone} />,
+    <Passo3
+      nextStep={nextStep}
+      prevStep={prevStep}
+      handleBuscarCep={handleBuscarCep}
+      uf={uf}
+      cidade={cidade}
+      bairro={bairro}
+      logradouro={logradouro}
+    />,
   ];
 
   return (
@@ -55,26 +89,26 @@ export default function Form() {
         </CSSTransition>
       </form>
     </div>
-  )
+  );
 }
 
-const Passo1 = ({ nextStep }) => (
+const Passo1 = ({ nextStep, rg, setRg, cpf, setCpf }) => (
   <div className={Styles.container_inputs}>
     <Email />
     <Senha />
     <Nome />
-    <RG />
-    <CPF />
+    <RG value={rg} setValue={setRg} /> {/* Passando rg e setRg como props */}
+    <CPF value={cpf} setValue={setCpf} />
     <br />
     <button type="button" onClick={nextStep} className={Styles.button}>
       Avançar
     </button>
   </div>
-)
+);
 
-const Passo2 = ({ nextStep, prevStep }) => (
+const Passo2 = ({ nextStep, prevStep, telefone, setTelefone }) => (
   <div className={Styles.container_inputs}>
-    <Telefone />
+    <Telefone value={telefone} setValue={setTelefone} />
     <DtNasc />
     <Genero />
     <DC />
@@ -85,15 +119,23 @@ const Passo2 = ({ nextStep, prevStep }) => (
       Avançar
     </button>
   </div>
-)
+);
 
-const Passo3 = ({ nextStep, prevStep }) => (
+const Passo3 = ({
+  nextStep,
+  prevStep,
+  handleBuscarCep,
+  uf,
+  cidade,
+  bairro,
+  logradouro,
+}) => (
   <div className={Styles.container_inputs}>
-    <Cep />
-    <UF />
-    <Cidade />
-    <Bairro />
-    <Rua />
+    <Cep onBuscarCep={handleBuscarCep} />
+    <UF u={uf} />
+    <Cidade c={cidade} />
+    <Bairro b={bairro} />
+    <Rua r={logradouro} />
     <br />
     <button type="button" onClick={prevStep} className={Styles.button}>
       Voltar
@@ -102,4 +144,4 @@ const Passo3 = ({ nextStep, prevStep }) => (
       Cadastrar
     </button>
   </div>
-)
+);
