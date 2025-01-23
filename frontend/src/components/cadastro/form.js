95cc9e9f640f98246ea27aa7fa638567133b8d08
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2025-01-23 17:00:54 -0300

    Trazendo trechos de codigos importantes para o novo Cadastro

--- frontend/src/components/cadastro/form.js
+++ frontend/src/components/cadastro/form.js
@@ -4,6 +4,7 @@ import { CSSTransition } from "react-transition-group";
 import Styles from "./form.module.css";
 import axios from "axios";
 
+// // Import dos Input
 import Nome from "../inputs_cadastro/nome_input";
 import Email from "../inputs_cadastro/email_input";
 import CPF from "../inputs_cadastro/cpf_input";
@@ -12,7 +13,6 @@ import RG from "../inputs_cadastro/rg_input";
 import Telefone from "../inputs_cadastro/telefone_input";
 import DtNasc from "../inputs_cadastro/dt_nasc_input";
 import DC from "../inputs_cadastro/destro_canhoto_input";
-
 
 // // Import dos Input de Endereço
 // import Cep from "../inputs_cadastro/endereco/cep_input";
@@ -23,12 +23,7 @@ import DC from "../inputs_cadastro/destro_canhoto_input";
 
 export default function Form(){
     const [step, setStep] = useState(0);
-    const [logradouro, setLogradouro] = useState("");
-    const [bairro, setBairro] = useState("");
-    const [cidade, setCidade] = useState("");
-    const [uf, setUf] = useState("");
-    const [cep, setCep] = useState("");
-    const [id_endereco, setEndereco] = useState(null);
+    const [responsePessoa, setResponsePessoa] = useState(null);
 
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
@@ -39,7 +34,7 @@ export default function Form(){
     const [nascimento, setNascimento] = useState("");
     const [mao_dominante, setMao_Dominante] = useState("");
 
-    const [responsePessoa, setResponsePessoa] = useState(null);
+    const [idade, setIdade] = useState("");
 
     const [nomeResp, setNomeResp] = useState("");
     const [emailResp, setEmailResp] = useState("");
@@ -48,7 +43,12 @@ export default function Form(){
     const [rgResp, setRgResp] = useState("");   
     const [telefoneResp, setTelefoneResp] = useState("");
 
-    const [idade, setIdade] = useState("");
+    const [logradouro, setLogradouro] = useState("");
+    const [bairro, setBairro] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [uf, setUf] = useState("");
+    const [cep, setCep] = useState("");
+    const [id_endereco, setEndereco] = useState(null);    
 
     const nextStep = () => {
         setStep((prevStep) => Math.min(prevStep + 1, steps.length - 1));
@@ -59,9 +59,121 @@ export default function Form(){
     };
         
     const steps = [
-      <Passo1 nextStep={nextStep} rg={rg} setRg={setRg} cpf={cpf} setCpf={setCpf} email={email} setEmail={setEmail} nome={nome} setNome={setNome} />,
-      <Passo2 prevStep={prevStep} telefoneResp={telefoneResp} setTelefoneResp={setTelefoneResp} nascimentoResp={nascimentoResp} setNascimentoResp={setNascimentoResp} generoResp={generoResp} setGeneroResp={setGeneroResp} cliquei={cliquei} />,
+      <Passo1 nextStep={nextStep} nome={nome} setNome={setNome} email={email} setEmail={setEmail} 
+        cpf={cpf} setCpf={setCpf} genero={genero} setGenero={setGenero} rg={rg} setRg={setRg} 
+        telefone={telefone} setTelefone={setTelefone} nascimento={nascimento} setNascimento={setNascimento} 
+        mao_dominante={mao_dominante} setMao_Dominante={setMao_Dominante}  calcularIdade={calcularIdade}/>,
+      <Passo2 prevStep={prevStep} />,
     ];
+
+    let { id_aluno } = useParams();
+    const navigate = useNavigate();
+
+    // useEffect(() => {
+    //   if (id_aluno !== undefined) {
+    //     logado();
+    //     id_aluno = parseInt(id_aluno);
+    //     preencherDados();
+    //   }
+    // }, [id_aluno]);
+
+    const calcularIdade = (dataNascimento) => {
+        const partes = dataNascimento.split('/');
+        
+        const hoje = new Date();
+        const nascimento = new Date(`${partes[2]}-${partes[1]}-${partes[0]}`);
+        let idadeCalculada = hoje.getFullYear() - nascimento.getFullYear();
+        const mes = hoje.getMonth();
+        if (mes < nascimento.getMonth() || (mes === nascimento.getMonth() && hoje.getDate() < nascimento.getDate())) {
+          idadeCalculada--;
+        }
+        setIdade(idadeCalculada);
+    };
+    const isUnder18 = () => {
+      console.log("Idade do aluno:", idade);  // Adicione este log para depuração
+      return idade < 18 && idade > 0;
+    }
+
+    const formatCPF = (cpf) => {
+        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    };
+
+    const formatRG = (rg) => {
+        return rg.replace(/(\d{2})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    };
+
+    function padraoBR(isoDate) {
+      const date = new Date(isoDate);
+      const day = String(date.getUTCDate()).padStart(2, '0');
+      const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Meses começam em 0
+      const year = date.getUTCFullYear();
+      return `${day}/${month}/${year}`;
+    }
+
+    const formatTelefone = (telefone) => {
+      telefone = telefone.replace(/\D/g, ''); // Remove non-digits
+      if (telefone.length === 11) {
+        return telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+      } else if (telefone.length === 10) {
+        return telefone.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+      }
+      return telefone;
+    };
+
+    const formatDate = (nascimento) => {
+      nascimento = nascimento.replace(/\D/g, '');
+      nascimento = nascimento.replace(/(\d{2})(\d{2})(\d)/, '$1/$2/$3');
+      return nascimento;
+    }
+
+    const convertDate = (date) => {
+      const [day, month, year] = date.split('/');
+      return `${year}-${month}-${day}`;
+    };
+
+//   const handleBuscarCep = (cep) => {
+//     if (cep.length < 9) {
+//       setLogradouro("");
+//       setBairro("");
+//       setCidade("");
+//       setUf("");
+//       return;
+//     }
+//     fetch(`https://viacep.com.br/ws/${cep}/json/`)
+//       .then((response) => response.json())
+//       .then((dados) => {
+//         setLogradouro(dados.logradouro);
+//         setBairro(dados.bairro);
+//         setCidade(dados.localidade);
+//         setUf(dados.uf);
+//       })
+//       .catch((error) => {
+//         console.error('Erro ao buscar CEP:', error);
+//       });
+//   };
+
+    const logado = async () => {
+      try {
+        let response = await axios.post('/login');
+        if (response.data.adm !== 1) {
+          navigate('/home');
+        }
+      } catch (error) {
+        navigate('/');
+      }
+    };
+
+    function tratamentoString(inputString) {
+      return inputString.replace(/[.\-()\s]/g, '');
+    }
+
+    // Obter a data atual
+    const dataAtual = new Date();
+    const adicionarZero = (numero) => (numero < 10 ? `0${numero}` : numero);
+    const ano = dataAtual.getFullYear();
+    const mes = adicionarZero(dataAtual.getMonth() + 1);
+    const dia = adicionarZero(dataAtual.getDate());
+    const dataFormatadaMySQL = `${ano}-${mes}-${dia}`;
 
     return(
         <div>
@@ -86,7 +198,7 @@ export default function Form(){
 }
 
 
-const Passo1 = ({ nextStep, email, setEmail, nome, setNome, rg, setRg, cpf, setCpf }) => (
+const Passo1 = ({ nextStep, calcularIdade, nome, setNome, email, setEmail, cpf, setCpf, genero, setGenero, rg, setRg, telefone, setTelefone, nascimento, setNascimento, mao_dominante, setMao_Dominante}) => (
     <div className={Styles.centro}>
         <div className={Styles.textcenter}>
           <h1>Dados do Aluno</h1>
@@ -95,24 +207,24 @@ const Passo1 = ({ nextStep, email, setEmail, nome, setNome, rg, setRg, cpf, setC
             <Nome value={nome} setValue={setNome}/>
             <Email value={email} setValue={setEmail}/>
 
-            <CPF />
-            <Genero />
+            <CPF value={cpf} setValue={setCpf}/>
+            <Genero value={genero} setValue={setGenero}/>
 
-            <RG />
-            <Telefone />
+            <RG value={rg} setValue={setRg}/>
+            <Telefone value={telefone} setValue={setTelefone}/>
 
-            <DtNasc />
-            <DC />
+            <DtNasc value={nascimento} setValue={setNascimento}/>
+            <DC value={mao_dominante} setValue={setMao_Dominante}/>
             <br />
             <button type="button" onClick={() => {
-            nextStep()
-            calcularIdade(nascimento)
+                nextStep()
+                calcularIdade(nascimento)
             }} className={Styles.button}>Avançar</button>
         </div>
     </div>
 );
 
-const Passo2 = ({ prevStep, nextStep, emailResp, setEmailResp, nomeResp, setNomeResp, rgResp, setRgResp, cpfResp, setCpfResp }) => (
+const Passo2 = ({ prevStep, nomeResp, setNomeResp, emailResp, setEmailResp, cpfResp, setCpfResp, generoResp, setGeneroResp, rgResp, setRgResp, telefoneResp, setTelefoneResp}) => (
   <div className={Styles.centro}>
     <div className={Styles.textcenter}>
       <h1>Dados do Responsável 1</h1>
@@ -129,7 +241,7 @@ const Passo2 = ({ prevStep, nextStep, emailResp, setEmailResp, nomeResp, setNome
 
         <br />
         <button type="button" onClick={prevStep} className={Styles.button}>Voltar</button>
-        <button type="button" onClick={cliquei} className={Styles.button}>Concluir</button>
+        <button type="button" onClick={alert("Para aqui")} className={Styles.button}>Concluir</button>
     </div>
   </div>
 );
