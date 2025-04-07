c58b620ca1d0ab5bc932fd7fac99c4529ead2cb5
saulobbarbosa <adventuresaulo@gmail.com>
2025-04-07 12:19:28 -0300

    Correção login

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -4,15 +4,18 @@ const axios = require("axios");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
+
+// routes/auth.js
 router.post("/login", async (req, res) => {
     const { usuario, senha } = req.body; 
+
     if (req.session.id_pessoa) {
         try {
             let id_pessoa = req.session.id_pessoa;
             const response = await axios.get('http://localhost:5000/api/pessoa');
             const dados = response.data;
             const pessoa = dados.find(p => p.id_pessoa === id_pessoa);
-            return res.json({nome: pessoa.nome_pessoa, adm: pessoa.adm });
+            return res.json({ nome: pessoa.nome_pessoa, adm: pessoa.adm });
         } catch (error) {
             return res.status(500).json({ message: "Erro ao buscar dados 1", error: error.message });
         }
@@ -21,9 +24,17 @@ router.post("/login", async (req, res) => {
             const response = await axios.get('http://localhost:5000/api/pessoa');
             const dados = response.data;
             const usuarioFormatado = usuario.toLowerCase();
+
             const pessoa = dados.find(p => {
-                const nomeCompleto = p.nome_pessoa.toLowerCase().split(" ");
-                const nomeUsuario = nomeCompleto.length > 1 ? `${nomeCompleto[0]}.${nomeCompleto[1]}` : nomeCompleto[0];
+                const partesNome = p.nome_pessoa.toLowerCase().split(" ");
+                let nomeUsuario = partesNome[0];
+
+                if (partesNome.length === 2) {
+                    nomeUsuario = `${partesNome[0]}.${partesNome[1]}`;
+                } else if (partesNome.length >= 3) {
+                    nomeUsuario = `${partesNome[0]}.${partesNome[partesNome.length - 1]}`;
+                }
+
                 return nomeUsuario === usuarioFormatado;
             });
 
@@ -38,13 +49,15 @@ router.post("/login", async (req, res) => {
             }
 
             req.session.id_pessoa = pessoa.id_pessoa;
-            return res.json({ nome: pessoa.nome_pessoa});
+            return res.json({ nome: pessoa.nome_pessoa });
 
         } catch (error) {
             return res.status(500).json({ message: "Erro ao buscar dados 2", error: error.message });
         }
     }
 });
+
+
 
 router.get('/sair', (req, res) => {
     req.session.destroy();
