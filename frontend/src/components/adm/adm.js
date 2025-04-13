01c88f382563fc158f9f9db382489e6cc58f66d0
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2025-04-13 19:32:39 -0300

    Modal com escolhas de cadastros

--- frontend/src/components/adm/adm.js
+++ frontend/src/components/adm/adm.js
@@ -10,6 +10,7 @@ import BarraLateral from "../barra_lateral/icons_barra_lateral";
 import Notifica from "../sino_notificacao/notificacao";
 
 export default function Adm() {
+    const [mostrar, setMostrar] = useState(false)
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -28,11 +29,34 @@ export default function Adm() {
         }
     };
 
+    const ApareceSome = () => {
+        setMostrar(!mostrar);
+    };
+
     return (
         <div>
             <Background_Sistema />
             <div className={ContainerCss.container}>
                 <BarraLateral />
+                {mostrar && (
+                    <>
+                        <div className={EstiloAdm.fundoEscuro} onClick={()=>ApareceSome()}></div>
+                        <div className={EstiloAdm.divCadastros}>
+                            <h1 className={EstiloAdm.titulo}>Cadastros</h1>
+                            <div>
+                                <button onClick={()=>navigate('/matricula')}>Alunos Pagantes</button>
+                                <button onClick={()=>navigate('/cadastro')}>Alunos Não Pagante</button>
+                                <button onClick={()=>navigate('/cadastro/aluno/Escola')}>Alunos de Escolas</button>
+                                <button onClick={()=>navigate('/cadastro/professor')}>Professores</button>
+                                <button onClick={()=>navigate('/cadastro/unidade')}>Unidades</button>
+                                <button onClick={()=>navigate('/cadastro/turma')}>Turmas</button>
+                            </div>
+                            <img src={require('../../imgs/icons/cancelar.png')} 
+                            className={EstiloAdm.imgFechar}
+                            onClick={()=>ApareceSome()}/>
+                        </div>
+                    </>
+                )}
                 <div className={EstiloAdm.container_adm}>
                     <div className={EstiloAdm.display_grid}>
                         <div className={EstiloAdm.cards_adm} onClick={()=>navigate('/adm/adm_aluno')}>
@@ -51,7 +75,7 @@ export default function Adm() {
                             <img src={require('../../imgs/icons/icon6.png')} alt="Turmas Img"/>
                             <h1>Turmas</h1>
                         </div>
-                        <div className={EstiloAdm.cards_adm} onClick={()=>alert("Belo Click Mano 🤓☝️")}>
+                        <div className={EstiloAdm.cards_adm} onClick={()=>ApareceSome()}>
                             <img src={require('../../imgs/icons/mais.png')} alt="Cadastro Img"/>
                             <h1>Cadastros</h1>
                         </div>
