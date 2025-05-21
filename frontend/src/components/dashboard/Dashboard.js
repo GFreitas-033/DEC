30a7b5012369fb2e1d5f1123540f229ebbd16227
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2025-05-21 20:10:24 -0300

    Criação do Container Principal e começo dos Blocos do Dashboard

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -8,14 +8,36 @@ import BarraLateral from "../barra-lateral/BarraLateral";
 import Notifica from "../sino-notificacao/Notificacao";
 import BtnVoltar from "../btn-voltar/BotaoVoltar";
 
+// Import dos Blocos do Dashboard
+import Geral from "./geral/AnaliseGeral"
+
 export default function Dashboard(){
     return(
         <div>
             <Background_Sistema />
             <div className={ContainerCss.container}>
                 <BarraLateral />
-                <div className={EstiloDashboard.nada}>
+                <div className={EstiloDashboard.ajusteTela}>
+                    <div className={EstiloDashboard.containerDash}>
+                        {/* <hr /> */}
+                        <Geral />
+                        <div className={EstiloDashboard.containerGraficos}>
 
+                        </div>
+                        <div className={EstiloDashboard.containerAnoNasc}>
+
+                        </div>
+                        <hr className={EstiloDashboard.hrContainer}/>
+                        <div className={EstiloDashboard.containerCidadeUnidade}>
+
+                        </div>
+                        <div className={EstiloDashboard.containerComparativo}>
+
+                        </div>
+                        <div className={EstiloDashboard.containerAlunosCidade}>
+
+                        </div>
+                    </div>
                 </div>
                 <Notifica />
                 <BtnVoltar />
---
+++ frontend/src/components/dashboard/geral/AnaliseGeral.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import EstiloGeral from "./geral.module.css"
+
+export default function Geral(){
+    return(
+        <div className={EstiloGeral.containerGeral}>
+            <h1 className={EstiloGeral.title}>Total de Alunos</h1>
+            <div className={EstiloGeral.part1}>
+                <span className={EstiloGeral.spanAluno}>5000</span>
+                <img src={require('../../../imgs/icons/group.png')} 
+                className={EstiloGeral.iconGroup} draggable="false" 
+                alt="Imagem Grupo"/>
+            </div>
+            <hr className={EstiloGeral.hrGeral}/>
+            <div className={EstiloGeral.bloquinhosGeral}>
+                <h2 className={EstiloGeral.definicao}>Ativos</h2>
+                <h2 className={EstiloGeral.resultado}>2500</h2>
+            </div>
+            <div className={EstiloGeral.bloquinhosGeral}>
+                <h2 className={EstiloGeral.definicao}>Inativos</h2>
+                <h2 className={EstiloGeral.resultado}>2500</h2>
+            </div>
+            <div className={EstiloGeral.bloquinhosGeral}>
+                <h2 className={EstiloGeral.definicao}>Pagante</h2>
+                <h2 className={EstiloGeral.resultado}>2500</h2>
+            </div>
+            <div className={EstiloGeral.bloquinhosGeral}>
+                <h2 className={EstiloGeral.definicao}>Não Pagante</h2>
+                <h2 className={EstiloGeral.resultado}>2500</h2>
+            </div>
+        </div>
+    )
+}
