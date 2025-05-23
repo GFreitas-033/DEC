0a4b225dc538626b837e00e2b929e9874320ff59
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2025-05-23 19:26:29 -0300

    Bloco de Alunos por Ano de Nascimento

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -11,6 +11,7 @@ import BtnVoltar from "../btn-voltar/BotaoVoltar";
 // Import dos Blocos do Dashboard
 import Geral from "./geral/AnaliseGeral"
 import Graficos from "./graficos/Graficos"
+import AnoNasc from "./ano-nasc/AnoNascimento"
 
 export default function Dashboard(){
     return(
@@ -23,6 +24,7 @@ export default function Dashboard(){
                         {/* <hr /> */}
                         <Geral />
                         <Graficos />
+                        <AnoNasc />
                     </div>
                 </div>
                 <Notifica />
