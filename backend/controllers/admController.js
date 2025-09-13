c7432d7737bb8b03dc90399ee37dcb9c3360221c
saulobbarbosa <adventuresaulo@gmail.com>
2025-09-13 12:24:06 -0300

    Retirada do axios do admController

--- backend/controllers/admController.js
+++ backend/controllers/admController.js
@@ -1,8 +1,12 @@
 // controllers/admController.js
 const express = require("express");
-const axios = require("axios");
 const router = express.Router();
 const alunoModel = require("../models/aluno");
+const professorModel = require("../models/professor");
+const pessoaModel = require("../models/pessoa");
+const turmaModel = require("../models/turma");
+const alunoHasTurmaModel = require("../models/aluno_has_turma");
+const unidadeModel = require("../models/unidade");
 
 router.get('/aluno', async (req, res) => {
     try {
@@ -17,7 +21,7 @@ router.get('/aluno', async (req, res) => {
 router.get('/excluiraluno/:id_aluno', async (req, res) => {
     const id_aluno = parseInt(req.params.id_aluno);
     try {
-        await axios.delete(`http://localhost:5000/api/aluno/${id_aluno}`);
+        await alunoModel.deleteAluno(id_aluno);
         res.status(200).send('Registro excluído com sucesso!');
     } catch (error) {
         console.error(error);
@@ -27,10 +31,10 @@ router.get('/excluiraluno/:id_aluno', async (req, res) => {
 
 router.get('/professor', async (req, res) => {
     try {
-        const responseProfessor = await axios.get('http://localhost:5000/api/professor');
+        const responseProfessor = await professorModel.readProfessor();
         const dadosProfessor = responseProfessor.data;
 
-        const responsePessoa = await axios.get('http://localhost:5000/api/pessoa');
+        const responsePessoa = await pessoaModel.readPessoa();
         const dadosPessoa = responsePessoa.data;
 
         const idsProfessor = new Set(dadosProfessor.map(professor => professor.id_pessoa));
@@ -47,8 +51,8 @@ router.get('/professor', async (req, res) => {
 router.get('/excluirprofessor/:id_professor', async (req, res) => {
     const id_professor = parseInt(req.params.id_professor);
     try {
-        await axios.delete(`http://localhost:5000/api/professor/${id_professor}`);
-        await axios.delete(`http://localhost:5000/api/pessoa/${id_professor}`);
+        await professorModel.deleteProfessor(id_professor);
+        await pessoaModel.deletePessoa(id_professor);
         res.status(200).send('Registro excluído com sucesso!');
     } catch (error) {
         console.error(error);
@@ -59,23 +63,23 @@ router.get('/excluirprofessor/:id_professor', async (req, res) => {
 router.get('/excluirunidade/:id_unidade', async (req, res) => {
     const id_unidade = parseInt(req.params.id_unidade);
     try {
-        const responseTurmas = await axios.get('http://localhost:5000/api/turma');
+        const responseTurmas = await turmaModel.readTurma();
         const dadosTurmas = responseTurmas.data;
         const turmasFiltradas = dadosTurmas.filter(turma => turma.id_unidade === id_unidade);
 
         const idsTurmasFiltradas = turmasFiltradas.map(turma => turma.id_turma);
 
-        const responseAlunos_has_Turmas = await axios.get('http://localhost:5000/api/aluno_has_turma');
+        const responseAlunos_has_Turmas = await alunoHasTurmaModel.readAlunoHasTurma();
         const dadosAlunos_has_Turmas = responseAlunos_has_Turmas.data;
         const alu_turmasFiltradas = dadosAlunos_has_Turmas.filter(alunos_has_turma => idsTurmasFiltradas.includes(alunos_has_turma.id_turma));
 
-        const promisesExcluirAlunos_has_Turmas = alu_turmasFiltradas.map(alunos_has_turma => axios.delete(`http://localhost:5000/api/aluno_has_turma/turma/${alunos_has_turma.id_turma}`));
+        const promisesExcluirAlunos_has_Turmas = alu_turmasFiltradas.map(alunos_has_turma => alunoHasTurmaModel.deleteAlunoHasTurma_Turma(alunos_has_turma.id_turma));
         await Promise.all(promisesExcluirAlunos_has_Turmas);
 
-        const promisesExcluirTurmas = turmasFiltradas.map(turma => axios.delete(`http://localhost:5000/api/turma/${turma.id_turma}`));
+        const promisesExcluirTurmas = turmasFiltradas.map(turma => turmaModel.deleteTurma(turma.id_turma));
         await Promise.all(promisesExcluirTurmas);
 
-        await axios.delete(`http://localhost:5000/api/unidade/${id_unidade}`);
+        await unidadeModel.deleteUnidade(id_unidade);
 
         res.status(200).json({
             message: 'Unidade e turmas associadas excluídas com sucesso!',
@@ -89,14 +93,14 @@ router.get('/excluirunidade/:id_unidade', async (req, res) => {
 router.get('/excluirturma/:id_turma', async (req, res) => {
     const id_turma = parseInt(req.params.id_turma);
     try {
-        const responseAlunos_has_Turmas = await axios.get('http://localhost:5000/api/aluno_has_turma');
+        const responseAlunos_has_Turmas = alunoHasTurmaModel.readAlunoHasTurma();
         const dadosAlunos_has_Turmas = responseAlunos_has_Turmas.data;
         const alu_turmasFiltradas = dadosAlunos_has_Turmas.filter(alunos_has_turma => alunos_has_turma.id_turma === id_turma);
 
-        const promisesExcluirAlunos_has_Turmas = alu_turmasFiltradas.map(alunos_has_turma => axios.delete(`http://localhost:5000/api/aluno_has_turma/turma/${alunos_has_turma.id_turma}`));
+        const promisesExcluirAlunos_has_Turmas = alu_turmasFiltradas.map(alunos_has_turma => alunoHasTurmaModel.deleteAlunoHasTurma_Turma(alunos_has_turma.id_turma));
         await Promise.all(promisesExcluirAlunos_has_Turmas);
 
-        await axios.delete(`http://localhost:5000/api/turma/${id_turma}`);
+        await turmaModel.deleteTurma(id_turma);
 
         res.status(200).json({
             message: 'Turma e associações de alunos excluídas com sucesso!',
